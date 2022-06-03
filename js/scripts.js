d3461c4bb6e896baf4cdc2bf022a64a70904b07f
Manuel Raygoza <Manny.raygoza@gmail.com>
2022-06-02 22:01:53 -0700

    WIP:Modified index.html and added +50 to quote.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,32 @@
+//$(document).ready(function() {
+  //$("form#insurance").submit(function(event) {
+    //const age = parseInt($("input#age").val());
+    //const gender = $("select#gender").val();
+
+    //const quote = (100 - age) * 3;
+
+    //$("#rate").text(quote);
+    //$("#quote").show();
+
+    //event.preventDefault();
+  //});
+//});
+
 $(document).ready(function() {
   $("form#insurance").submit(function(event) {
     const age = parseInt($("input#age").val());
     const gender = $("select#gender").val();
 
-    const quote = (100 - age) * 3;
+    let quote = (100 - age) * 3;
+    if (gender === 'male' && age < 26) {
+      quote += 50;
+    }
 
     $("#rate").text(quote);
-    $("#quote").show();
-
+    $("#quote").show()
+  } else {
+    alert('Please enter your age.');
+  }
     event.preventDefault();
   });
 });
